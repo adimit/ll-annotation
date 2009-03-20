b453f8b4f8cde9446e04694d0af2b7e765342b78
Александър Димитров <aleks_d@gmx.de>
2009-03-20 13:16:49 +0100

    Added tokenize' to the API, haddock comments.
    We now can tokenize to Strings as well as to ByteStrings.

--- src/Tokenizer.hs
+++ src/Tokenizer.hs
@@ -1,20 +1,26 @@
 module Tokenizer 
-        (tokenize) 
+        (tokenize, tokenize') 
         where
 
 import Prelude hiding (either)
 import qualified Data.ByteString.Lazy.Char8 as C
 
+-- Tokenize a ByteString to a list of ByteStrings.
+tokenize :: C.ByteString -> [C.ByteString]
+tokenize = C.groupBy token
 
-tokenize :: C.ByteString -> [String]
-tokenize = map C.unpack . C.groupBy token
+-- Tokenize a ByteString to a list of Strings.
+tokenize' :: C.ByteString -> [String]
+tokenize' = map C.unpack . C.groupBy token
 
+-- A helper function for tokenize's C.groupBy.
 token :: Char -> Char -> Bool
 token ' ' ' ' = True
 token '\'' ' ' = False
 token ' ' '\'' = False
 token x y = not . or $ map (oneOf (x,y)) [' ',',','.','!','?','\'','"','-']
 
+-- Is one of (x,y) a z?
 oneOf :: (Char,Char) -> Char -> Bool
 oneOf (x,y) z | x == z  = True
               | y == z  = True
