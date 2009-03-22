3eecb96024a091e6a0f10560daa491f21cb35d52
Александър Димитров <aleks.dimitrov@gmail.com>
2009-03-22 21:22:23 +0100

    Added possibility to serialize tokens from corpus.
    And also to display them. We can now read in a file in the corpus format
    and view its source in the GTK textView component (given it's been
    converted with the new TextToCorpus tool.)

--- src/Annotator/Interface.hs
+++ src/Annotator/Interface.hs
@@ -1,5 +1,5 @@
 module Annotator.Interface ( runGUI ) where
-     
+
 import Annotator.Tokenizer
 import Annotator.DTD
 import Annotator.Interface.Constants
@@ -11,25 +11,9 @@ import Text.PrettyPrint.HughesPJ (render)
 import Text.XML.HaXml.Types
 import Text.XML.HaXml.Pretty (document)
 import Text.XML.HaXml.XmlContent (toXml)
+import Text.XML.HaXml.XmlContent.Haskell (readXml)
 import qualified Data.ByteString.Lazy.Char8 as C
 
--- Takes a set of tokens as Strings and transforms them to an XML document
-createXML :: [String] -> Document ()
-createXML xs = toXml False (Corpus tokens (Errors []))
-        where tokens = mkTokens xs
-
--- Take tokenized text and turn it into a <tokens/> element
-mkTokens :: [String] -> Tokens
-mkTokens = Tokens . gentoks 0
-         where gentoks :: Int -> [String] -> [Token]
-               gentoks _ [] = []
-               gentoks x (t:ts) = (Token attrs t) : gentoks (x+1) ts
-                       where attrs = Token_Attrs { tokenIdx = "t" ++ show x }
-
--- render an XML document
-prettyXML ::  Document () -> String
-prettyXML = render . document 
-                        
 -- helper function to associate all menu items with actions
 initControls :: GladeXML -> IO ()
 initControls xml = do quitItem <- xmlGetWidget xml castToMenuItem menuItemQuit
@@ -50,28 +34,36 @@ openFileAction window xml = do
               _ ->putStr "" -- any better way of no-op in IO ()?
     widgetHide fc
 
-
 loadFile :: GladeXML -> String -> IO ()
 loadFile xml fn = do putStrLn $ "Opening File: " ++ fn
                      ta <- xmlGetWidget xml castToTextView corpusView
                      tb <- textViewGetBuffer ta
                      content <- readFile fn
-                     textBufferSetText tb content
+                     let corpus = readXml content
+                     case corpus of
+			  Left  s -> showError s
+			  Right s -> do tb `textBufferSetText` (xmlToTokenString s)
 
+xmlToTokenString :: Corpus -> String
+xmlToTokenString (Corpus (Tokens xs) _)= tokenListToString xs
 
+tokenListToString :: [Token] -> String
+tokenListToString = concat . map tokenToString
+	where tokenToString (Token _ s) = s
 
+showError = undefined
 
 constructOpenFileChooser :: Window -> IO FileChooserDialog
 constructOpenFileChooser w = do 
-       fc <- fileChooserDialogNew (Just "Open Corpus")  
+       fc <- fileChooserDialogNew (Just "Open Corpus")
                                   (Just w) 
-                                  FileChooserActionOpen  
+                                  FileChooserActionOpen
                                   [("gtk-cancel",ResponseCancel),("gtk-open",ResponseAccept)]
        fileChooserSetSelectMultiple fc False
        ff <- xmlFileFilter
-       fileChooserAddFilter fc ff                  
+       fileChooserAddFilter fc ff
        return fc
-       
+
 runGUI :: IO ()
 runGUI = do initGUI
             Just xml <- xmlNew gladeSource
