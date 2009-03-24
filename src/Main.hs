0e688a0dd0ab1230b3b19109b54516d168b48594
Александър Димитров <aleks.dimitrov@gmail.com>
2009-03-24 01:45:24 +0100

    Added command line processing for main module.

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,11 @@
 module Main where
     
 import Annotator.Interface
+import System.Environment (getArgs)
 
 main :: IO ()
-main = runGUI
+main = do args <- getArgs
+          case args of 
+               [] -> runGUI
+               [fn] -> runGUIWithFile fn
+               _    -> putStrLn "Please provide either exactly one file name or no arguments."
