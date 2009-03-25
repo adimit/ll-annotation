43522b34f09ce2372db4020fe8b45d896750d4d0
Александър Димитров <aleks.dimitrov@gmail.com>
2009-03-25 05:07:05 +0100

    Correct signal handling in TextView component.

--- src/Annotator/Interface.hs
+++ src/Annotator/Interface.hs
@@ -18,44 +18,23 @@ module Annotator.Interface
 import Annotator.DTD
 import Annotator.Interface.Constants
 import Control.Monad.Trans (liftIO)
+import Data.IORef
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Gdk.EventM
 import Graphics.UI.Gtk.Glade
 import Graphics.UI.Gtk.Windows.Dialog
 import Text.XML.HaXml.XmlContent.Haskell (readXml)
 
--- Helper function to associate all controls with actions
-initControls :: GladeXML -> IO ()
-initControls xml = do quitItem <- xmlGetWidget xml castToMenuItem menuItemQuit
-                      openItem <- xmlGetWidget xml castToMenuItem menuItemOpen
-                      window   <- xmlGetWidget xml castToWindow windowMain
-                      quitItem `afterActivateLeaf` do widgetDestroy window
-                      openItem `afterActivateLeaf` do fn <- openFileAction window
-                                                      case fn of
-                                                           (Just fn) -> loadFile xml fn
-                                                           _         -> return ()
-                      return ()
+-- | GUI state
+data Gui = Gui { corpusView  :: TV -- ^ The corpusView
+               , window      :: Window -- ^ The main window
+               , xml         :: GladeXML -- ^ The underlying glade XML
+               , corpusClick :: Maybe (ConnectId TextView) -- ^ The onClick event on corpusView
+               }
 
--- Queries the user for opening a file.
-openFileAction :: Window -> IO (Maybe FilePath)
-openFileAction window = do
-    fc <- constructOpenFileChooser window
-    r  <- dialogRun fc
-    widgetHide fc
-    case r of
-         ResponseAccept -> fileChooserGetFilename fc
-         _              -> return Nothing
-
--- Load a corpus from a file and display it.
-loadFile :: GladeXML -> String -> IO ()
-loadFile xml fn = do putStrLn $ "Opening File: " ++ fn
-                     ta <- xmlGetWidget xml castToTextView corpusView
-                     tb <- textViewGetBuffer ta
-                     content <- readFile fn
-                     let corpus = readXml content
-                     case corpus of
-                          Left  s -> showError s
-                          Right s -> do tb `textBufferSetText` (xmlToTokenString s)
+data TV = TV { tv :: TextView
+             , i :: IORef (Maybe (ConnectId TextView))
+             }            
 
 -- Takes a corpus and returns a string representing the corpus' text in plain text.
 xmlToTokenString :: Corpus -> String
@@ -66,10 +45,10 @@ xmlToTokenString (Corpus (Tokens xs) _)= concat . map tokenToString $ xs
 showError = undefined
 
 -- Builds a GTK file Chooser to open files.
-constructOpenFileChooser :: Window -> IO FileChooserDialog
-constructOpenFileChooser w = do 
+constructOpenFileChooser :: Gui -> IO FileChooserDialog
+constructOpenFileChooser gui = do
     fc <- fileChooserDialogNew (Just "Open Corpus")
-                               (Just w)
+                               (Just (window gui))
                                FileChooserActionOpen
                                [("gtk-cancel",ResponseCancel),("gtk-open",ResponseAccept)]
     fileChooserSetSelectMultiple fc False
@@ -84,49 +63,90 @@ xmlFileFilter = do ff <- fileFilterNew
                    fileFilterAddMimeType ff "text/xml"
                    return ff
 
--- Initialize the program's widget's events, listeners, and event handlers.
-initWidgets :: GladeXML -> IO ()
-initWidgets xml = do tv <- xmlGetWidget xml castToTextView corpusView
-                     l  <- xmlGetWidget xml castToLabel "label1"
-                     tv `on` buttonPressEvent $ findContext tv l
-                     return ()
-
-findContext :: TextView -> Label -> EventM EButton Bool
-findContext tv l = do btn <- eventButton
-                      case btn of
-                           LeftButton -> do coords <- eventCoordinates
-                                            let (x,y) = truncCoordToInt coords
-                                            liftIO $ do bcrd <- tv `textViewWindowToBufferCoords` TextWindowWidget $ (x,y)
-                                                        iter <- uncurry (textViewGetIterAtLocation tv) $ bcrd
-                                                        iter' <- textIterCopy iter
-                                                        textIterBackwardChars iter 5
-                                                        textIterForwardChars iter' 5
-                                                        slice <- textIterGetSlice iter iter'
-                                                        l `labelSetText` slice
-                                                        return False
-                           _          -> return False
+findContext :: Gui -> Label -> EventM EButton Bool
+findContext gui l = do btn <- eventButton
+                       case btn of
+                            LeftButton -> do coords <- eventCoordinates
+                                             let (x,y) = truncCoordToInt coords
+                                             liftIO $ do bcrd <- (tv (corpusView gui)) `textViewWindowToBufferCoords` TextWindowWidget $ (x,y)
+                                                         iter <- uncurry (textViewGetIterAtLocation (tv(corpusView gui))) $ bcrd
+                                                         iter' <- textIterCopy iter
+                                                         textIterBackwardChars iter 5
+                                                         textIterForwardChars iter' 5
+                                                         slice <- textIterGetSlice iter iter'
+                                                         l `labelSetText` slice
+                                                         putStrLn slice
+                                                         return False
+                            _          -> return False
 
 truncCoordToInt :: (Double,Double) -> (Int,Int)
 truncCoordToInt (x,y) = (truncate x,truncate y)
 
 -- | Entry in to the GUI
 runGUI :: IO ()
-runGUI = do prepareGUI
-            mainGUI
+runGUI = prepareGUI >> mainGUI
 
 -- | Entry in to the GUI, whilst opening a corpus file.
 runGUIWithFile :: FilePath -> IO ()
-runGUIWithFile fn = do xml <-  prepareGUI
-                       loadFile xml fn
+runGUIWithFile fn = do gui <-  prepareGUI
+                       loadFile gui fn
                        mainGUI
 
 -- Common code used by GUI entry points, factored out. Returns the GladeXML of the main GUI.
-prepareGUI :: IO GladeXML
-prepareGUI = do initGUI
-                Just xml <- xmlNew gladeSource
-                window <- xmlGetWidget xml castToWindow windowMain
-                initControls xml
-                initWidgets xml
-                onDestroy window mainQuit
-                widgetShowAll window
-                return xml
+prepareGUI :: IO Gui
+prepareGUI = do
+    initGUI
+    mXml <- xmlNew gladeSource
+    case mXml of
+         Nothing       -> showError $ "Invalid glade xml file " ++ gladeSource
+         Just gladeXml -> do w      <- xmlGetWidget gladeXml castToWindow windowMain
+                             textView <- xmlGetWidget gladeXml castToTextView "corpusView"
+                             nothingRef <- newIORef Nothing
+                             let gui = Gui { corpusView  = TV textView nothingRef
+                                           , corpusClick = Nothing
+                                           , window      = w
+                                           , xml         = gladeXml
+                                           }
+                             initControls gui
+                             onDestroy w mainQuit
+                             widgetShowAll w
+                             return gui
+
+-- Helper function to associate all controls with actions
+initControls :: Gui -> IO ()
+initControls gui = do quitItem <- xmlGetWidget (xml gui) castToMenuItem menuItemQuit
+                      openItem <- xmlGetWidget (xml gui) castToMenuItem menuItemOpen
+                      quitItem `afterActivateLeaf` do widgetDestroy (window gui)
+                      openItem `afterActivateLeaf` do fn <- openFileAction gui
+                                                      case fn of
+                                                           (Just fn') -> loadFile gui fn'
+                                                           _          -> return ()
+                      return ()
+
+-- Queries the user for opening a file.
+openFileAction :: Gui -> IO (Maybe FilePath)
+openFileAction gui = do
+    fc <- constructOpenFileChooser gui
+    r  <- dialogRun fc
+    widgetHide fc
+    case r of
+         ResponseAccept -> fileChooserGetFilename fc
+         _              -> return Nothing
+
+-- Load a corpus from a file, display it, and set the appropriate events.
+loadFile :: Gui -> String -> IO ()
+loadFile gui fn = do l  <- xmlGetWidget (xml gui) castToLabel "label1"
+                     tb <- textViewGetBuffer (tv (corpusView gui))
+                     content <- readFile fn
+                     let corpus = readXml content
+                     case corpus of
+                          Left  s -> showError $ "XML Parsing failed. " ++ s
+                          Right s -> do tb `textBufferSetText` (xmlToTokenString s)
+                     ref <- readIORef (i (corpusView gui))
+                     case ref of
+                          (Just i') -> signalDisconnect i'
+                          Nothing   -> return ()
+                     connectId <- (tv (corpusView gui)) `on` buttonPressEvent $ findContext gui l
+                     writeIORef (i (corpusView gui)) $ Just connectId
+                     return ()
+
