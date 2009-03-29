cf5244088bb73dadb6c4e0832f8b72be71585924
Александър Димитров <aleks.dimitrov@gmail.com>
2009-03-29 13:55:16 +0200

    Factored out data types.

--- src/Annotator/Interface.hs
+++ src/Annotator/Interface.hs
@@ -17,6 +17,7 @@ module Annotator.Interface
 
 import Annotator.DTD
 import Annotator.Interface.Constants
+import Annotator.Interface.Types
 import Control.Monad.Trans (liftIO)
 import Data.IORef
 import Data.List
@@ -39,33 +40,6 @@ data Gui = Gui { corpusView  :: TextView -- ^ The corpusView
                , selectedTkn :: IORef (Maybe [Token])
                , currentErrs :: IORef (Maybe [Error])
                }
-
-type TokenMap = Map Span Token
-
-data Span = Span Int Int
-          | Point Int 
-          deriving (Show)
-
-instance Eq Span where
-    x /= y = not $ x == y
-    (Span x1 s1) == (Span x2 s2) | x1 == x2 && s1 == s2 = True
-                                 | otherwise            = False
-    (Point p1)   == (Point p2)   | p1 == p2             = True
-                                 | otherwise            = False
-    (Span x s)   == (Point p)    | p >= x && p <= (x+s)   = True
-                                 | otherwise            = False
-    p@(Point _)  == s@(Span _ _) = s == p
-    
-instance Ord Span where
-    compare (Span x1 s1) (Span x2 s2) | x1 < x2 = LT
-                                      | x2 < x1 = GT
-                                      | x1 == x2 = compare s1 s2
-                                      | otherwise = error "This shouldn't happen."
-    compare (Span x s) (Point p) | p < x     = LT
-                                 | p > (x+s) = GT
-                                 | otherwise = EQ
-    compare p@(Point _) s@(Span _ _) = compare s p
-    compare (Point p1) (Point p2) = compare p1 p2
 
 -- Takes a corpus and returns a string representing the corpus' text in plain text.
 xmlToTokenString :: Corpus -> (Int,String,TokenMap)
---
+++ src/Annotator/Interface/Types.hs
@@ -0,0 +1,42 @@
+-- | A small module to contain all relevant data types for the Interface.
+
+module Types where
+
+-- | GUI state
+data Gui = Gui { corpusView  :: TextView -- ^ The corpusView
+               , window      :: Window -- ^ The main window
+               , xml         :: GladeXML -- ^ The underlying glade XML
+               , tokenLabel  :: Label -- ^ The Label displaying tokens
+               , corpusClick :: IORef (Maybe (ConnectId TextView))
+               , tokens      :: IORef (Maybe TokenMap)
+               , selectedTkn :: IORef (Maybe [Token])
+               , currentErrs :: IORef (Maybe [Error])
+               }
+
+type TokenMap = Map Span Token
+
+data Span = Span Int Int
+          | Point Int 
+          deriving (Show)
+
+instance Eq Span where
+    x /= y = not $ x == y
+    (Span x1 s1) == (Span x2 s2) | x1 == x2 && s1 == s2 = True
+                                 | otherwise            = False
+    (Point p1)   == (Point p2)   | p1 == p2             = True
+                                 | otherwise            = False
+    (Span x s)   == (Point p)    | p >= x && p <= (x+s)   = True
+                                 | otherwise            = False
+    p@(Point _)  == s@(Span _ _) = s == p
+    
+instance Ord Span where
+    compare (Span x1 s1) (Span x2 s2) | x1 < x2 = LT
+                                      | x2 < x1 = GT
+                                      | x1 == x2 = compare s1 s2
+                                      | otherwise = error "This shouldn't happen."
+    compare (Span x s) (Point p) | p < x     = LT
+                                 | p > (x+s) = GT
+                                 | otherwise = EQ
+    compare p@(Point _) s@(Span _ _) = compare s p
+    compare (Point p1) (Point p2) = compare p1 p2
+
