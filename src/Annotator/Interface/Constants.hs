9c0005bbfe5507e4b23ad7bd087510052fa2771c
Александър Димитров <aleks.dimitrov@gmail.com>
2009-04-04 20:59:18 +0200

    Forgot to put file in Repo: Constants.
    This will probably be deprecated soon anyway, but as of now, it's a
    compile time dependency.

---
+++ src/Annotator/Interface/Constants.hs
@@ -0,0 +1,16 @@
+-- A small module to hold constant mappings should the names in the
+-- glade source file have to change.
+
+module Annotator.Interface.Constants where
+
+gladeSource = "gui/AnnotatorGUI.glade"
+windowMain  = "mainWindow"
+
+-- menu items
+menuItemOpen = "menuItemOpen"
+menuItemQuit = "menuItemQuit"
+ 
+-- file chooser
+fcOpenDialog   = "openDialog"
+fcCancelButton = "fcCancelButton"
+fcOpenButton   = "fcOpenButton"
