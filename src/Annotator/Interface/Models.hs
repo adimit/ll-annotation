f06c54d09722bee611851e96a913e634cb08f097
Александър Димитров <aleks.dimitrov@gmail.com>
2009-04-04 21:44:46 +0200

    Changed EType to reflect tree-ish data structure.
    This should also work for the cell construction with fancy cells. Just
    put in another constructor like ENodeTrigger, with its own functions and
    all that buzz.

--- src/Annotator/Interface/Models.hs
+++ src/Annotator/Interface/Models.hs
@@ -10,26 +10,30 @@ import Graphics.UI.Gtk.Glade
 import Graphics.UI.Gtk.ModelView
 import Text.XML.HaXml.XmlContent
 
-data EType = forall a b. XmlContent (a,b) => EType { name :: String , payload :: (Maybe a -> b) }
+data EType =forall a b. XmlContent (a,b) =>
+                  ENode { name :: String, payload :: (Maybe a -> b) }
+           |forall a. XmlContent a =>
+                  ELeaf { name :: String, leaf :: a }
+
                                                   
 leafNode :: a -> Tree.Tree a
 leafNode a = Tree.Node a []
 
 errorStore :: IO (TreeStore EType)
 errorStore = treeStoreNew 
-    [ Tree.Node  (EType "Error" (Error (Error_Attrs Nothing)))
-      [ Tree.Node (EType "Spelling" (\t -> Error_Spelling (Spelling t)))
-        [ leafNode $ EType "Homonymy" (leafType $ (Spelling_Homonymy Homonymy)) 
-        , leafNode $ EType "Register" (leafType $ (Spelling_Register Register)) 
-        , leafNode $ EType "Diacritics" (leafType $ (Spelling_Diacritics Diacritics)) 
-        , leafNode $ EType "Local" (leafType $ (Spelling_Local Local)) 
+    [ Tree.Node  (ENode "Error" (\t -> Error (Error_Attrs Nothing) t))
+      [ Tree.Node (ENode "Spelling" (\t -> Error_Spelling (Spelling t)))
+        [ leafNode $ ELeaf "Homonymy" (Spelling_Homonymy Homonymy)
+        , leafNode $ ELeaf "Register" (Spelling_Register Register) 
+        , leafNode $ ELeaf "Diacritics" (Spelling_Diacritics Diacritics) 
+        , leafNode $ ELeaf "Local" (Spelling_Local Local) 
         ]
-      , leafNode $ EType "Grammar" (\t -> Error_Grammar (Grammar (Grammar_Attrs Nothing) t))
-      , Tree.Node (EType "Context" (\t -> Error_Context (Context t)))
-        [ leafNode $ EType "Lexical" (leafType $ (Context_Lexical Lexical)) 
-        , leafNode $ EType "Idiom" (leafType $ (Context_Idiom Idiom)) 
+      , leafNode $ ENode "Grammar" (\t -> Error_Grammar (Grammar (Grammar_Attrs Nothing) t))
+      , Tree.Node (ENode "Context" (\t -> Error_Context (Context t)))
+        [ leafNode $ ELeaf "Lexical" (Context_Lexical Lexical) 
+        , leafNode $ ELeaf "Idiom" (Context_Idiom Idiom)
         ]
-      , leafNode $ EType "Gibberish" (leafType $ (Error_Gibberish Gibberish))
+      , leafNode $ ELeaf "Gibberish" (Error_Gibberish Gibberish)
       ]
    ]
 
